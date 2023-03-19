8eecaff34d918e4968cb4a76447b8f572b995736
ContentGamer <85808595+ContentGamer@users.noreply.github.com>
2023-03-19 15:39:25 +0000

    Initial commit

---
+++ timelib.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+#include <sstream>
+#include <stdlib.h>
+
+using namespace std;
+
+int getHours(){
+    char tm[] = __TIME__;
+    char *hours = strtok(tm, " ");
+    return stoi(hours);
+}
+int getMinutes(){
+    char tm[] = __TIME__;
+    char *minutes = strtok(tm, " ");
+    minutes = strtok(NULL, " ");
+    return stoi(minutes);
+}
+int getSeconds(){
+    char tm[] = __TIME__;
+    char *seconds = strtok(tm, " ");
+    seconds = strtok(NULL, " ");
+    seconds = strtok(NULL, " ");
+    return stoi(seconds);
+}
